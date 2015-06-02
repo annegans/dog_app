09ac6b0de422d06188c53f60afe8fd0a6ceed7ad
Anne Ganzeboom <anneganzeboom@gmail.com>
2015-06-02 17:43:38 +0100

    you can add a new dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,4 +3,25 @@ class DogsController < ApplicationController
   def index  
     @dogs = Dog.all
   end
+
+  def show 
+     @dog = Dog.find(params[:id])
+  end
+
+  def new 
+    @dog = Dog.new
+  end
+ 
+ def create
+  Dog.create(dog_params)
+  ridirect_to(dogs.path)
+ end
+
+ private 
+
+  def dog_params
+    # This is a security method that allows us to update 
+    params.require(:dog).permit(:name, :breed, :age, :color)
+  end
+
 end
